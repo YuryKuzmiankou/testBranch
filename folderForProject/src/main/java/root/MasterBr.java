dc8605c2320a4bc734f825f0c6d36283142c8d19
Yury Kuzmiankou <yury_kuzmiankou@epam.com>
2019-07-25 10:37:43 +0300

    branch-4 comm.-1

--- folderForProject/src/main/java/root/MasterBr.java
+++ folderForProject/src/main/java/root/MasterBr.java
@@ -3,5 +3,6 @@ package root;
 public class MasterBr {
     public static void main(String[] args) {
         System.out.println("Hello Branch-1!");
+        System.out.println("Branch-4 comm.-1 from last commit master!");
     }
 }
