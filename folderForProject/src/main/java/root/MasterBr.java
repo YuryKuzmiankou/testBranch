bab34b7bc3978f0a550c9d0a160215d9aa71d775
Yury Kuzmiankou <yury_kuzmiankou@epam.com>
2019-07-25 10:34:06 +0300

    branch-5 comm.-1

--- folderForProject/src/main/java/root/MasterBr.java
+++ folderForProject/src/main/java/root/MasterBr.java
@@ -3,5 +3,6 @@ package root;
 public class MasterBr {
     public static void main(String[] args) {
         System.out.println("Hello Master!");
+        System.out.println("From master: comm.-1");
     }
 }
